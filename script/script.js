b79b6a87053b00b53d10b05c36773d73644606be
Erinaldo Junior <erinaldogomesjr@gmail.com>
2022-12-28 15:54:58 +0000

    att

---
+++ script/script.js
@@ -0,0 +1,19 @@
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll("a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+scrollSuave();
