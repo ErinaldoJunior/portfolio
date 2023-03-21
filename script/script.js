544ee5493dda0f377c12d2ff0450ba407dc8dff5
Erinaldo Junior <erinaldogomesjr@gmail.com>
2023-03-21 16:34:33 +0000

    att menu mobile

--- script/script.js
+++ script/script.js
@@ -282,6 +282,7 @@ const menu = document.querySelector('.menu-mobile');
 function ativarMenu(event) {
   event.preventDefault();
   menu.classList.toggle('menu-ativo');
+  menuLogo.classList.toggle('logo-ativa');
 }
 
 menuLogo.addEventListener('click', ativarMenu);
