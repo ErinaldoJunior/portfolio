8b37731ccfe09d517cb1d862990b0ab8fdf4c31f
Erinaldo Junior <erinaldogomesjr@gmail.com>
2023-03-22 11:52:54 +0000

    att intro

--- script/script.js
+++ script/script.js
@@ -286,3 +286,17 @@ function ativarMenu(event) {
 }
 
 menuLogo.addEventListener('click', ativarMenu);
+
+//////////////////////////////////////////////////////////////////////////
+
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
