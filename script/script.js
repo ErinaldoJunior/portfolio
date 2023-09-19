81aaa11f1db33c5a76911a2931884f9ff7df1b11
Erinaldo Junior <erinaldogomesjr@gmail.com>
2023-09-19 16:37:50 +0100

    att imagens

--- script/script.js
+++ script/script.js
@@ -3,16 +3,16 @@ function scrollSuave() {
 
   function scrollToSection(event) {
     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
+    const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
   }
 
   linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
+    link.addEventListener("click", scrollToSection);
   });
 }
 
@@ -21,13 +21,13 @@ scrollSuave();
 //////////////////////////////////////////////////////////////////////////
 
 function typeWrite(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = ' ';
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
   textoArray.forEach(function (letra, i) {
     setTimeout(function () {
       elemento.innerHTML += letra;
     }, 100 * i);
   });
 }
-const titulo = document.querySelector('.titulo-principal');
+const titulo = document.querySelector(".titulo-principal");
 typeWrite(titulo);
